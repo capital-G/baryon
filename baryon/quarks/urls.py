d5b52fa6d95c5c705dd67540c03b0f70819a2119
Dennis Scheiba <dennis.scheiba@gmx.de>
2024-06-11 15:07:16 +0200

    use TemplateView for static pages

--- baryon/quarks/urls.py
+++ baryon/quarks/urls.py
@@ -4,11 +4,12 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    path("", cache_page(1)(views.index), name="index"),
+    path("", cache_page(1)(views.IndexView.as_view()), name="index"),
     path("quarks", views.QuarksListView.as_view(), name="quarks"),
     path("extensions", views.ExtensionListView.as_view(), name="extensions"),
     path("classes", views.ClassesListView.as_view(), name="classes"),
-    path("about", cache_page(1)(views.about), name="about"),
+    path("docs", views.DocsListView.as_view(), name="docs"),
+    path("about", cache_page(1)(views.AboutView.as_view()), name="about"),
     # cache this as this has dependencies scanning
     path(
         "project/<str:name>",
